e278b2fb0dd0b02d4a8069706bebb24000b99e52
kitsoRik <gamedevrostik@gmail.com>
2020-07-18 22:26:43 +0300

    Added tests for objects

---
+++ src/__tests__/utils/objects.test.ts
@@ -0,0 +1,41 @@
+import { compareObjects, hashFromObject } from '../../utils/objects';
+
+describe('objects', () => {
+	it('compareObjects', () => {
+		expect(compareObjects({}, {})).toBeTruthy();
+
+		expect(compareObjects({}, { name: 'string' })).toBeFalsy();
+
+		expect(compareObjects({ name: 'string' }, {})).toBeFalsy();
+
+		expect(
+			compareObjects({ name: 'string' }, { name: 'string' })
+		).toBeTruthy();
+
+		expect(
+			compareObjects(
+				{ name: 'string', age: 19 },
+				{ name: 'string', age: 19 }
+			)
+		).toBeTruthy();
+
+		expect(
+			compareObjects(
+				{ age: 19, name: 'string' },
+				{ name: 'string', age: 19 }
+			)
+		).toBeTruthy();
+	});
+
+	it('hashFromObject', () => {
+		expect(hashFromObject({})).toEqual(hashFromObject({}));
+		expect(hashFromObject({ name: '' })).not.toEqual(hashFromObject({}));
+		expect(hashFromObject({})).not.toEqual(hashFromObject({ name: '' }));
+		expect(hashFromObject({ name: '' })).toEqual(
+			hashFromObject({ name: '' })
+		);
+		expect(hashFromObject({ name: '', age: 19 })).toEqual(
+			hashFromObject({ age: 19, name: '' })
+		);
+	});
+});
--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,7 +1,61 @@
 export const compareObjects = (obj1: object, obj2: object): boolean => {
-	return JSON.stringify(obj1) === JSON.stringify(obj2);
+	return compareObjectRecursive(obj1, obj2);
+};
+
+const compareObjectRecursive = (obj1: object, obj2: object): boolean => {
+	let flag = true;
+
+	const keys1 = Object.keys(obj1);
+	const keys2 = Object.keys(obj2);
+
+	if (keys1.length !== keys2.length) return false;
+
+	for (let i = 0; i < keys1.length; i++) {
+		const key = keys1[i];
+		const v1 = obj1[key];
+		const v2 = obj2[key];
+
+		if (typeof v1 === 'object') {
+			if (typeof v2 !== 'object') return false;
+
+			if (v1 === undefined) {
+				if (v2 !== undefined) return false;
+			}
+
+			if (v1 === null) {
+				if (v2 !== null) return false;
+			}
+
+			flag = compareObjectRecursive(v1, v2);
+		} else {
+			flag = v1 === v2;
+		}
+
+		if (flag === false) return false;
+	}
+
+	return true;
 };
 
 export const hashFromObject = (obj: object) => {
-	return JSON.stringify(obj);
+	return JSON.stringify(hashFromObjectRecursive(obj));
+};
+
+const hashFromObjectRecursive = (obj: object) => {
+	const keys = Object.keys(obj).sort();
+
+	const result: any = {};
+
+	for (let i = 0; i < keys.length; i++) {
+		const key = keys[i];
+		const value = obj[key];
+
+		if (typeof value === 'object') {
+			result[key] = hashFromObjectRecursive(value);
+		} else {
+			result[key] = obj[key];
+		}
+	}
+
+	return result;
 };
