d92ca15a9844ba3d0f8bae3f288324258fa199b0
kitsoRik <gamedevrostik@gmail.com>
2020-07-18 21:07:09 +0300

    Added some types to useLocationField

--- src/hooks/useLocationField/useLocationField.ts
+++ src/hooks/useLocationField/useLocationField.ts
@@ -2,16 +2,17 @@ import { InitialExtendValue } from '../../types/Initial/Initial';
 import { QueryValue } from '../../types/Query';
 import useLocationQueryExtend from '../useLocationQueryExtend';
 
-const useLocationField = <T = QueryValue>(
+const useLocationField = <T extends QueryValue>(
 	name: string,
 	value?: InitialExtendValue
-): [T, (value: any) => void] => {
+): [T, (value: T) => void] => {
 	const { fullQuery, setQueryField } = useLocationQueryExtend(
 		value ? { [name]: value } : {}
 	);
+
 	return [
 		(fullQuery[name] as any) as T,
-		(value: any) => setQueryField(name, value)
+		(value: T) => setQueryField(name, value)
 	];
 };
 
