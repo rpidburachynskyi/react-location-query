ec4781345b393e0cd3280d7758eaa6014cbc709e
kitsoRik <gamedevrostik@gmail.com>
2020-07-19 20:26:56 +0300

    Updated example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, Typography, Row, Col } from 'antd';
 import CreateQueryList from './CreateQueryList';
 import { inject, observer } from 'mobx-react';
-import { QueryItems } from './mobx/models/QueryItems';
 import AddQueryItemDialog from './AddQueryItemDialog';
 import QueryList from './QueryList';
-import {
-	setOptions,
-	useLocationField,
-	useLocationQueryExtend
-} from 'react-location-query';
+import { setOptions } from 'react-location-query';
+import { QueryItems } from './mobx/models/QueryItems';
 
 setOptions({
 	removeUnusedQueryFields: true
@@ -19,67 +15,30 @@ interface Props {
 	queryItems?: QueryItems;
 }
 
-function App() {
-	const [name, setName] = useLocationField('name', {
-		type: 'number',
-		initial: 123,
-		// @ts-ignore
-		onParsedError: (q) => {
-			return 32;
-		}
-	});
+const App = ({ queryItems }: Props) => {
+	const [addQueryDialogVisible, setAddQueryDialogVisible] = useState(false);
 
-	useEffect(() => {
-		setName(228);
-	}, []);
-
-	console.log(name);
-
-	const [isIndex, setIsIndex] = useState(true);
 	return (
-		<div className='App'>
-			{isIndex && <NonIndexComponent />}
-			{!isIndex && <IndexComponent />}
-			<button onClick={() => setIsIndex(!isIndex)}>
-				Toggle indexes components
-			</button>
+		<div>
+			<Typography.Title>React hook - useLocationQuery</Typography.Title>
+			<Row gutter={[8, 8]}>
+				<Col span={12}>
+					<CreateQueryList />
+					<Button onClick={() => setAddQueryDialogVisible(true)}>
+						Add query item
+					</Button>
+					{addQueryDialogVisible && (
+						<AddQueryItemDialog
+							onClose={() => setAddQueryDialogVisible(false)}
+						/>
+					)}
+				</Col>
+				<Col span={12}>
+					<QueryList />
+				</Col>
+			</Row>
 		</div>
 	);
-}
-
-const IndexComponent = () => {
-	const [name] = useLocationField<any>('name');
-	return <h1>My name is {name.value}</h1>;
 };
-
-const NonIndexComponent = () => {
-	return <h1>I'm without index</h1>;
-};
-
-// const App = ({ queryItems }: Props) => {
-// 	const [addQueryDialogVisible, setAddQueryDialogVisible] = useState(false);
-
-// 	return (
-// 		<div>
-// 			<Typography.Title>React hook - useLocationQuery</Typography.Title>
-// 			<Row gutter={[8, 8]}>
-// 				<Col span={12}>
-// 					<CreateQueryList />
-// 					<Button onClick={() => setAddQueryDialogVisible(true)}>
-// 						Add query item
-// 					</Button>
-// 					{addQueryDialogVisible && (
-// 						<AddQueryItemDialog
-// 							onClose={() => setAddQueryDialogVisible(false)}
-// 						/>
-// 					)}
-// 				</Col>
-// 				<Col span={12}>
-// 					<QueryList />
-// 				</Col>
-// 			</Row>
-// 		</div>
-// 	);
-// };
 
 export default inject('queryItems')(observer(App));
