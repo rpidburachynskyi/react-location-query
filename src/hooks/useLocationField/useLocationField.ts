bcf6b72c4e84bdee88e2c222e0e8053573adc7f2
kitsoRik <gamedevrostik@gmail.com>
2020-07-21 20:41:10 +0300

    Added types for useLocationField

--- src/hooks/useLocationField/useLocationField.ts
+++ src/hooks/useLocationField/useLocationField.ts
@@ -1,19 +1,55 @@
-import { InitialExtendValue } from '../../types/Initial/Initial';
-import { QueryValue } from '../../types/Query';
+import {
+	InitialExtendObjectArrayBoolean,
+	InitialExtendObjectArrayNumber,
+	InitialExtendObjectArrayString
+} from '../../types/Initial/Array';
+import { InitialExtendObjectBoolean } from '../../types/Initial/Boolean';
+import { InitialExtendObjectJson } from '../../types/Initial/Json';
+import { InitialExtendObjectNumber } from '../../types/Initial/Number';
+import { InitialExtendObjectString } from '../../types/Initial/String';
 import useLocationQueryExtend from '../useLocationQueryExtend';
 
-const useLocationField = <T extends QueryValue>(
+function useLocationField(
 	name: string,
-	value?: InitialExtendValue
-): [T, (value: T) => void] => {
+	value?: InitialExtendObjectString | string
+): [string, (value: string) => void];
+
+function useLocationField(
+	name: string,
+	value?: InitialExtendObjectNumber | number
+): [number, (value: number) => void];
+
+function useLocationField(
+	name: string,
+	value?: InitialExtendObjectBoolean | boolean
+): [boolean, (value: boolean) => void];
+
+function useLocationField(
+	name: string,
+	value?: InitialExtendObjectJson | object
+): [object, (value: object) => void];
+
+function useLocationField(
+	name: string,
+	value?: InitialExtendObjectArrayBoolean
+): [boolean[], (value: boolean[]) => void];
+
+function useLocationField(
+	name: string,
+	value?: InitialExtendObjectArrayNumber
+): [number[], (value: number[]) => void];
+
+function useLocationField(
+	name: string,
+	value?: InitialExtendObjectArrayString
+): [string[], (value: string[]) => void];
+
+function useLocationField(name: string, value?: any) {
 	const { fullQuery, setQueryField } = useLocationQueryExtend(
-		value ? { [name]: value } : {}
+		value !== undefined ? { [name]: value } : {}
 	);
 
-	return [
-		(fullQuery[name] as any) as T,
-		(value: T) => setQueryField(name, value)
-	];
-};
+	return [fullQuery[name], (value: any) => setQueryField(name, value)] as any;
+}
 
 export default useLocationField;
