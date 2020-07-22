491b80e81701d6c20786f7320adf49ceeca1b12a
kitsoRik <gamedevrostik@gmail.com>
2020-07-22 21:01:07 +0300

    Added enum to string and example with it

--- examples/src/examples/Enum/Enum.tsx
+++ examples/src/examples/Enum/Enum.tsx
@@ -4,9 +4,9 @@ import { useLocationField } from 'react-location-query';
 const Enum = () => {
 	const [type, setType] = useLocationField('type', {
 		type: 'string',
-		initial: 'asd',
-		enum: ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'],
-		onParsedEnumError: () => 'a'
+		initial: 'qwe',
+		enum: ['first', 'second', 'third', 'fourth', 'fifth'],
+		onParsedEnumError: () => 'first'
 	});
 	return (
 		<div>
@@ -16,6 +16,26 @@ const Enum = () => {
 				<div>
 					<button onClick={() => setType('first')}>
 						set 'first'
+					</button>
+					<button onClick={() => setType('second')}>
+						set 'second'
+					</button>
+					<button onClick={() => setType('third')}>
+						set 'third'
+					</button>
+					<button onClick={() => setType('fourth')}>
+						set 'fourth'
+					</button>
+					<button onClick={() => setType('fifth')}>
+						set 'fifth'
+					</button>
+				</div>
+			</div>
+			<div>
+				<h2>Not allow types (will replace to 'first')</h2>
+				<div>
+					<button onClick={() => setType('sixth')}>
+						set 'sixth'
 					</button>
 				</div>
 			</div>
--- src/types/Initial/String.ts
+++ src/types/Initial/String.ts
@@ -3,6 +3,6 @@ import { _InitialExtendObject } from './Initial';
 export interface InitialExtendObjectString extends _InitialExtendObject {
 	type: 'string';
 	initial: string;
-	enum?: [string];
+	enum?: string[];
 	onParsedEnumError?: (value: string) => string;
 }
