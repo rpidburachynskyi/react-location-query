e6b7bd84a3271a792a20e1993d70d4e9a6656b7b
kitsoRik <gamedevrostik@gmail.com>
2020-07-22 08:51:23 +0300

    Removed unusable import useState from App.tsx

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import Medium from './examples/Medium';
 import Simple from './examples/Simple';
