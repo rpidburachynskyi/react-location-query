a809b74d19a1ee68a252ba6e6e943b6d663648f0
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-08-14 22:11:28 +0300

    Fixed check initial string

--- src/lib/utils/valuesController/checkInitialValue/checkInitialValueString.ts
+++ src/lib/utils/valuesController/checkInitialValue/checkInitialValueString.ts
@@ -5,12 +5,21 @@ const checkInitialValueString = (initialValue: ObjectString) => {
 		throw new Error('Initial value must be a string');
 
 	if (initialValue.enum !== undefined) {
-		if (initialValue.enum.length === 0)
-			throw new Error('Enum array cannot be empty');
-		if (!initialValue.enum.includes(initialValue.initial))
-			throw new Error(
-				`Initial value is not exists in enum array, expected: ${initialValue.enum}, got: ${initialValue.initial}`
-			);
+		if (Array.isArray(initialValue.enum)) {
+			if (initialValue.enum.length === 0)
+				throw new Error('Enum array cannot be empty');
+			if (!initialValue.enum.includes(initialValue.initial))
+				throw new Error(
+					`Initial value is not exists in enum array, expected: ${initialValue.enum}, got: ${initialValue.initial}`
+				);
+		} else {
+			if (initialValue.enum.array.length === 0)
+				throw new Error('Enum array cannot be empty');
+			if (!initialValue.enum.array.includes(initialValue.initial))
+				throw new Error(
+					`Initial value is not exists in enum.array, expected: ${initialValue.enum.array}, got: ${initialValue.initial}`
+				);
+		}
 	}
 };
 
